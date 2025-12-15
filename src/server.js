58bc06ea8a32209c7e3a585f64ee3a81938885b0
Alasiuidi <alaeddine.souidi@esprit.tn>
2025-12-15 23:28:19 +0100

    fix express

--- src/server.js
+++ src/server.js
@@ -2,27 +2,28 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-const db = require('./db'); 
-
+const db = require('./db');
 
 const app = express();
-
-app.use(cors({ origin: "http://localhost:5173" }));
-app.use(express.json());
-app.use('/', routes);
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
+// Routes
+app.use('/', routes);
+
+// Bootstrap server
 (async () => {
   try {
     await db.getPool();
     console.log('Database initialized.');
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
   } catch (err) {
     console.error('Failed to initialize database:', err);
     process.exit(1);
